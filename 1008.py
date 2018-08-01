5a8fe45c08500fa775f200345e1b909355469a34
arturbs <arturbritosouza@hotmail.com.br>
2018-07-31 23:24:25 -0300

    adicionando exercicios 1001 e 1008

---
+++ 1008.py
@@ -0,0 +1,17 @@
+number = int(input())
+
+
+hours = int(input())
+
+
+sph = float(input())
+
+
+
+salario = float(hours * sph)
+
+
+
+print("NUMBER = %d" %number)
+
+print("SALARY = U$ %0.2f" %salario)
